65073947c077aad0e3703033a90af11b6c36c48a
apagyiedavid <apagyiek@gmail.com>
2025-11-16 23:04:30 +0000

    commit

--- alx-react-app-new/src/components/Footer.jsx
+++ alx-react-app-new/src/components/Footer.jsx
@@ -1,6 +1,13 @@
 function Footer() {
   return (
-    <footer>
+    <footer style = {{
+    backgroundColor: "#071629",
+    color: "#fff",
+    padding: "15px",
+    textAlign: "center",
+    marginTop: "30px",
+    borderTop: "3px solid #ff6565"
+  }}>
   <p>© 2023 City Lovers</p>
 </footer>
   )
--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,6 +1,13 @@
 function MainContent() {
   return (
-    <main>
+    <main style ={{
+    backgroundColor: "#f4f4f4",
+    padding: "30px",
+    borderRadius: "10px",
+    textAlign: "center",
+    marginTop: "20px",
+    boxShadow: "0 2px 8px rgba(0,0,0,0.1)"
+  }}>
       <p>I love to visit New York, Paris, and Tokyo.</p>
     </main>
   )
