e7e7c942ae9977ef1ba5e62e9353128de5b48bde
apagyiedavid <apagyiek@gmail.com>
2025-11-16 18:58:23 +0000

    first commit

---
+++ alx-react-app/src/components/UserProfile.jsx
@@ -0,0 +1,14 @@
+// src/App.jsx
+function UserProfile() {
+  return (
+    <div>
+      <UserProfile 
+        name="Alice" 
+        age="25" 
+        bio="Loves hiking and photography" 
+      />
+    </div>
+  );
+}
+
+export default UserProfile ;
