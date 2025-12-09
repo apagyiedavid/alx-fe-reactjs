0273ab1b60f9f5f9b93619590aebddca9c405817
apagyiedavid <apagyiek@gmail.com>
2025-12-09 19:53:01 +0000

    commit again

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -2,8 +2,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./components/HomePage";
 import RecipeDetail from "./components/RecipeDetail";
 import AddRecipeForm from "./components/AddRecipeForm";
-
-
 function App() {
   return (
     <Router>
--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -13,7 +13,7 @@ function HomePage() {
   return (
     <div className="min-h-screen bg-blue-100 p-6">
       <h1 className="text-3xl font-bold text-center text-blue-700 mb-8">
-      Recipe Sharing Platform
+      King Dave Recipe Sharing Platform
       </h1>
 
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3 max-w-6xl mx-auto">
@@ -39,10 +39,17 @@ function HomePage() {
 
               <Link
                 to={`/recipe/${recipe.id}`}
-                className="text-green-600 font-medium hover:underline"
+                className="text-yellow-600 font-medium hover:underline hover:text-red-600"
               >
                 View Recipe →
               </Link>
+              <Link
+               to="/add"
+               className="bg-green-600 text-white px-4 py-2 rounded-md hover:bg-green-700"
+               >
+               Add New Recipe
+               </Link>
+
             </div>
           </div>
         ))}
